17b0421f2b62763bec9be8f35b0f5e8dd2e8cd64
mayuresh <mayureshraval2012@gmail.com>
2021-02-03 00:35:07 +0530

    Initial Commit

--- app/src/main/java/com/codewithmischief/tempowala/Joinus.java
+++ app/src/main/java/com/codewithmischief/tempowala/Joinus.java
@@ -3,12 +3,23 @@ package com.codewithmischief.tempowala;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Joinus extends AppCompatActivity {
-
+Button mSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joinus);
+        mSubmit=findViewById(R.id.submit);
+
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Joinus.this, "Submitting..", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
