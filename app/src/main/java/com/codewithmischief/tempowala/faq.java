fd5389efd743c84dc66561908be4027a83a41d2b
mayuresh <mayureshraval2012@gmail.com>
2021-02-05 00:42:55 +0530

    Initial Commit

--- app/src/main/java/com/codewithmischief/tempowala/faq.java
+++ app/src/main/java/com/codewithmischief/tempowala/faq.java
@@ -5,17 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class faq extends AppCompatActivity {
-TextView mfaq,mAns;
+TextView mfaq,mAns,mfaq2,mAns2,mfaq3,mAns3,mfaq4,mAns4,mfaq5,mAns5,mfaq6,mAns6,mfaq7,mAns7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
 
-        mfaq=findViewById(R.id.faq);
+        mfaq=findViewById(R.id.faq1);
         mAns=findViewById(R.id.ans);
+        mfaq2=findViewById(R.id.faq2);
+        mAns2=findViewById(R.id.ans2);
+        mfaq3=findViewById(R.id.faq3);
+        mAns3=findViewById(R.id.ans3);
+        mfaq4=findViewById(R.id.faq4);
+        mAns4=findViewById(R.id.ans4);
+        mfaq5=findViewById(R.id.faq5);
+        mAns5=findViewById(R.id.ans5);
+        mfaq6=findViewById(R.id.faq6);
+        mAns6=findViewById(R.id.ans6);
+        mfaq7=findViewById(R.id.faq7);
+        mAns7=findViewById(R.id.ans7);
 
 //        mfaq.setOnClickListener(new View.OnClickListener() {
 //            int counter=0; //setting counter to count onclick
@@ -33,12 +46,83 @@ TextView mfaq,mAns;
             @Override
             public void onClick(View view) {
                 if(mAns.getVisibility()== View.VISIBLE){
+//                    ViewGroup.MarginLayoutParams marginParams = (ViewGroup.MarginLayoutParams) mfaq2.getLayoutParams();
+//                    marginParams.setMargins(marginParams.leftMargin,
+//                            20, //setting it back to 0
+//                            marginParams.rightMargin,
+//                            marginParams.bottomMargin);
+
                     mAns.setVisibility(View.GONE);
                 }
                 else
                     mAns.setVisibility(View.VISIBLE);
+//                ViewGroup.MarginLayoutParams marginParams = (ViewGroup.MarginLayoutParams) mfaq2.getLayoutParams();
+//                marginParams.setMargins(marginParams.leftMargin,
+//                        400, //only changing top margin
+//                        marginParams.rightMargin,
+//                        marginParams.bottomMargin);
+            }
+        });
+        mfaq2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAns2.getVisibility()== View.VISIBLE){
+                    mAns2.setVisibility(View.GONE);
+                }
+                else
+                    mAns2.setVisibility(View.VISIBLE);
             }
         });
 
+        mfaq3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAns3.getVisibility()== View.VISIBLE){
+                    mAns3.setVisibility(View.GONE);
+                }
+                else
+                    mAns3.setVisibility(View.VISIBLE);
+            }
+        });
+        mfaq4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAns4.getVisibility()== View.VISIBLE){
+                    mAns4.setVisibility(View.GONE);
+                }
+                else
+                    mAns4.setVisibility(View.VISIBLE);
+            }
+        });
+        mfaq5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAns5.getVisibility()== View.VISIBLE){
+                    mAns5.setVisibility(View.GONE);
+                }
+                else
+                    mAns5.setVisibility(View.VISIBLE);
+            }
+        });
+        mfaq6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAns6.getVisibility()== View.VISIBLE){
+                    mAns6.setVisibility(View.GONE);
+                }
+                else
+                    mAns6.setVisibility(View.VISIBLE);
+            }
+        });
+        mfaq7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAns7.getVisibility()== View.VISIBLE){
+                    mAns7.setVisibility(View.GONE);
+                }
+                else
+                    mAns7.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
